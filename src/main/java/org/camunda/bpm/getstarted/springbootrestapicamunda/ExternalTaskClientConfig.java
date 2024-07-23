48694d5d8145405746dfca33a7e705a7bfe4204a
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-23 17:07:52 +0330

    classes to config the external tasks and engine added and work well ! the project (process) is deploying well and without error

---
+++ src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/ExternalTaskClientConfig.java
@@ -0,0 +1,18 @@
+package org.camunda.bpm.getstarted.springbootrestapicamunda;
+
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ExternalTaskClientConfig {
+
+    @Bean
+    public ExternalTaskClient externalTaskClient() {
+        return ExternalTaskClient.create()
+                .baseUrl("http://localhost:8080/engine-rest")
+                .asyncResponseTimeout(20000)
+                .build();
+    }
+}
