50e54bc05d2d1b07f6b742e340290df49a9e788d
ruzstock.ir <ali16mar15couples@gmail.com>
2024-07-29 12:56:48 +0330

    in the ClientConfig class added a bean for rest template class !!! rest template class send and retrieve data from camunda engine !

--- src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/ClientConfig.java
+++ src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/ClientConfig.java
@@ -4,9 +4,15 @@ import org.camunda.bpm.client.ExternalTaskClient;
 import org.camunda.bpm.client.spring.annotation.EnableExternalTaskClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     @Bean
     public ExternalTaskClient externalTaskClient() {
