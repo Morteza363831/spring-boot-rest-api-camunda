a1ab36f8046cb42a3bf3d46fc2941f1e1d2541a7
ruzstock.ir <ali16mar15couples@gmail.com>
2024-08-03 16:57:41 +0330

    in controller , we added three methods two get and one post . in get methods we render form io and display it to user and in post method we send retrieved data to sprint-boot-rest-camunda project to start process and do process after all we send updated data using response entity to spring-boot-rest-form-io project to display it to user !

--- src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/CamundaController.java
+++ src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/CamundaController.java
@@ -3,10 +3,7 @@ package org.camunda.bpm.getstarted.springbootrestapicamunda;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +28,7 @@ public class CamundaController {
         return "form";
     }
 
-    @PostMapping("/submitForm")
+    /*@PostMapping("/submitForm")
     public String submitForm(@ModelAttribute("user") User user, Model model) {
         String processDefinitionKey = "serviceTask"; // Ensure this matches your BPMN definition key
         String startProcessUrl = CAMUNDA_BASE_URL + "/process-definition/key/" + processDefinitionKey + "/start";
@@ -88,7 +85,7 @@ public class CamundaController {
             return "error";
         }
     }
-
+*/
 
     @GetMapping("/displayUser")
     public String displayUser(@RequestParam("processInstanceId") String processInstanceId, Model model) {
@@ -108,12 +105,86 @@ public class CamundaController {
             return "error";
         }
     }
-
-
     private Map<String, Object> createVariableMap(Object value, String type) {
         Map<String, Object> variable = new HashMap<>();
         variable.put("value", value);
         variable.put("type", type);
         return variable;
+    }
+
+
+    @PostMapping("/submit-form")
+    public ResponseEntity<Map<String,Object>> handleFormSubmission(@RequestBody Map<String, Object> formData) {
+        // get data
+        String name = formData.get("name").toString();
+        Integer age = Integer.valueOf(formData.get("age").toString());
+        String job = formData.get("job").toString();
+
+        // initial process definition 
+        String processDefinitionKey = "serviceTask"; // Ensure this matches your BPMN definition key
+        String startProcessUrl = CAMUNDA_BASE_URL + "/process-definition/key/" + processDefinitionKey + "/start";
+
+        // Start process instance and set initial variables
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("name", createVariableMap(name, "String"));
+        variables.put("age", createVariableMap(age, "Integer"));
+        variables.put("job", createVariableMap(job, "String"));
+
+        // add variables to http entity and then send it to start process url
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("variables", variables);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
+
+        try {
+            // call rest template to send rest api to uri
+            ResponseEntity<Map> response = restTemplate.postForEntity(startProcessUrl, request, Map.class);
+            Map<String, Object> responseBody = response.getBody();
+
+            // response statement
+            if (response.getStatusCode().is2xxSuccessful() && responseBody != null) {
+                String processInstanceId = (String) responseBody.get("id");
+                logger.info("Process instance started with ID: {}", processInstanceId);
+
+                // Fetch the task for the process instance
+                String tasksUrl = CAMUNDA_BASE_URL + "/task?processInstanceId=" + processInstanceId;
+                Task[] tasks = restTemplate.getForObject(tasksUrl, Task[].class);
+
+                if (tasks != null && tasks.length > 0) {
+                    String taskId = tasks[0].getId();
+                    logger.info("Task fetched with ID: {}", taskId);
+
+                    // Complete the task
+                    String completeTaskUrl = CAMUNDA_BASE_URL + "/task/" + taskId + "/complete";
+                    HttpEntity<String> completeTaskRequest = new HttpEntity<>("", headers);
+
+                    restTemplate.postForObject(completeTaskUrl, completeTaskRequest, String.class);
+                    logger.info("Task completed with ID: {}", taskId);
+
+                    // get all variables form process
+                    String url = CAMUNDA_BASE_URL + "/process-instance/" + processInstanceId + "/variables";
+                    Map<String, Map<String, Object>> newVariables = restTemplate.getForObject(url, Map.class);
+                    Map<String,Object> data = new HashMap<>();
+                    data.put("userName", newVariables.get("newName").get("value"));
+                    data.put("userAge", newVariables.get("newAge").get("value"));
+                    data.put("userJob", newVariables.get("newJob").get("value"));
+
+                    return ResponseEntity.ok(data);
+
+                } else {
+                    logger.error("No tasks found for process instance ID: {}", processInstanceId);
+                    return ResponseEntity.noContent().build();
+                }
+            } else {
+                logger.error("Failed to start process instance. Response: {}", responseBody);
+                return ResponseEntity.noContent().build();
+            }
+        } catch (Exception e) {
+            logger.error("Exception occurred: ", e);
+            return ResponseEntity.noContent().build();
+        }
     }
 }
