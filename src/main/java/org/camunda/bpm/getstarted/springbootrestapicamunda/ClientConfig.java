1d165ff28d9e598fd708128b28227bd0d6b3702b
ruzstock.ir <ali16mar15couples@gmail.com>
2024-08-04 16:20:58 +0330

    comments added to class

--- src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/ClientConfig.java
+++ src/main/java/org/camunda/bpm/getstarted/springbootrestapicamunda/ClientConfig.java
@@ -1,19 +1,21 @@
 package org.camunda.bpm.getstarted.springbootrestapicamunda;
 
 import org.camunda.bpm.client.ExternalTaskClient;
-import org.camunda.bpm.client.spring.annotation.EnableExternalTaskClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfig {
 
+    // rest template bean
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    // make a task client to make connection with camunda external engine
     @Bean
     public ExternalTaskClient externalTaskClient() {
         return ExternalTaskClient.create()
